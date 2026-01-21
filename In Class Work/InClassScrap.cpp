3ba72e94fef6bc90c91ec217fb818cda9582d681
rileyashok <rileyashok@gmail.com>
2026-01-21 12:18:57 -0500

    Initial commit: EECE2140

---
+++ In Class Work/InClassScrap.cpp
@@ -0,0 +1,15 @@
+// Examine C++ and predict outcome
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = 7;
+
+    cout << endl;
+    cout << x+ y << " " << x * y;
+    cout << endl;
+
+    return 0;
+}
---
+++ In Class Work/InClassWork2.cpp
@@ -0,0 +1,46 @@
+/* 
+This is a simple C++ program that asks the user for the number of elements in a secret sequence
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {  //Asks for numbers in the secret sequence
+   /*
+    //defining variables
+    int n1;
+    int n2=++n1;
+
+    //inputs and outputs
+    cout<<n1<<" "<<n2<<endl;
+   
+   
+    cout<<"How many Numbers are in the Secret Sequence?"<<endl;
+    cin >> n;
+
+    //receives the number inputs for the secret sequence
+    for(int i=0; i<n; i++) 
+    {
+        
+        int code;
+       
+        cout<<"Enter a Code "<<i+1<<": ";
+        cin >> code;
+        
+
+       // cout<<"Hello"<<endl;
+    }
+
+    return 0;
+    */
+
+    int num;
+    int tempNum;
+    num=18;
+    tempNum=2*num;
+
+    cout<<"Num = "<<num<<", tempNum = "<<tempNum<<endl;
+
+    return 0;
+}
+
